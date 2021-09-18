bbfef3c30a15d7d9eb2b3b185f0a7a91064de4e9
Umar Yousafzai <umaryousafzai54@gmail.com>
2021-09-18 10:27:33 -0400

    get backend started

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,20 @@
-rom flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from flask_restful import Api
 import requests
 import json
 
-from . import services
+# from . import services
 
 app = Flask(__name__)
 app.debug=True
 CORS(app)
 api = Api(app)
+
+
+@app.route('/')
+def index(): 
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
