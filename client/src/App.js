4ca4fb1610db9b0ada7e24590d8d89c4e0064ab1
Ryan <ryanyan@rogers.com>
2021-09-18 22:11:39 -0400

    got the background stuff working

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,39 @@ import { TableContainer } from '@mui/material';
 import NameField from './Components/NameField'
 import Navbar from './Components/NavBar';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import background1 from './Assets/Capture1.PNG';
+import background2 from './Assets/Capture2.PNG';
+import background3 from './Assets/Capture3.PNG';
+import zIndex from '@mui/material/styles/zIndex';
 
 function App() {
+  var background = background1;
+  const x = Math.floor(Math.random()*3);
+  if (x == 1) {
+    background = background2;
+  }
+  else if (x == 2) {
+    background = background3;
+  }
   return (
     <Router>
-    <div className="App">
+      <div style={{
+        backgroundImage: 'url('+background+')',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        filter: 'blur(2px)'
+      }}>
+        
+      </div>
+    <div className="App" style={{
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      zIndex: 2,
+      height: '100%',
+      width: '100%'
+      }}>
         <Navbar/>
         <br/>
         <Route path='/about' />
